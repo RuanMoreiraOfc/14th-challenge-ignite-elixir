8252b5b1224224b83fdefe5c2a20a3ea1b11a8d3
Ruan Moreira de Jesus <ruanmoreiraofc@hotmail.com>
2022-06-30 13:59:08 -0300

    feat: create `Github` response

---
+++ lib/expi/github/response.ex
@@ -0,0 +1,29 @@
+defmodule Expi.Github.Response do
+  @keys [
+    :id,
+    :name,
+    :description,
+    :html_url,
+    :stargazers_count
+  ]
+
+  @enforce_keys @keys
+
+  defstruct @keys
+
+  def build(%{
+        "id" => id,
+        "name" => name,
+        "description" => description,
+        "html_url" => html_url,
+        "stargazers_count" => stargazers_count
+      }) do
+    %__MODULE__{
+      id: id,
+      name: name,
+      description: description,
+      html_url: html_url,
+      stargazers_count: stargazers_count
+    }
+  end
+end
