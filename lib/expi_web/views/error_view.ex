37244ef205c701d69fbd28e394d52ea8c7411167
Ruan Moreira de Jesus <ruanmoreiraofc@hotmail.com>
2022-07-01 12:21:14 -0300

    feat(ErrorView): treat errors

--- lib/expi_web/views/error_view.ex
+++ lib/expi_web/views/error_view.ex
@@ -1,6 +1,10 @@
 defmodule ExpiWeb.ErrorView do
   use ExpiWeb, :view
 
+  import Ecto.Changeset, only: [traverse_errors: 2]
+
+  alias Ecto.Changeset
+
   # If you want to customize a particular status code
   # for a certain format, you may uncomment below.
   # def render("500.html", _assigns) do
@@ -12,5 +16,21 @@ defmodule ExpiWeb.ErrorView do
   # "Not Found".
   def template_not_found(template, _assigns) do
     Phoenix.Controller.status_message_from_template(template)
+  end
+
+  def render("error.json", %{result: %Changeset{} = result}) do
+    %{message: translate_errors(result)}
+  end
+
+  def render("error.json", %{result: result}) do
+    %{message: result}
+  end
+
+  def translate_errors(changeset) do
+    traverse_errors(changeset, fn {msg, opts} ->
+      Enum.reduce(opts, msg, fn {key, result}, acc ->
+        String.replace(acc, "%{#{key}}", to_string(result))
+      end)
+    end)
   end
 end
