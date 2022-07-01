091f0c9e0236f0bff386f99572c74b7399f1d188
Ruan Moreira de Jesus <ruanmoreiraofc@hotmail.com>
2022-07-01 10:56:02 -0300

    chore: create mock for `GithubClient`

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,2 +1,6 @@
+{:ok, _} = Application.ensure_all_started(:mox)
 ExUnit.start()
 Ecto.Adapters.SQL.Sandbox.mode(Expi.Repo, :manual)
+
+# MOX
+Mox.defmock(Expi.Github.ClientMock, for: Expi.Github.Behaviour)
