733757f8eb40133afa9354d34b9989067418bb95
Ruan Moreira de Jesus <ruanmoreiraofc@hotmail.com>
2022-07-01 10:57:43 -0300

    test: add "renders repos.json" case

--- test/expi_web/views/repos_view_test.exs
+++ test/expi_web/views/repos_view_test.exs
@@ -1,3 +1,28 @@
 defmodule ExpiWeb.ReposViewTest do
   use ExpiWeb.ConnCase, async: true
+
+  import Phoenix.View
+
+  alias ExpiWeb.ReposView
+
+  test "renders repos.json" do
+    repos = [
+      %{
+        id: 1,
+        name: "name",
+        html_url: "html_url",
+        description: "description",
+        stargazers_count: 0
+      },
+      %{
+        id: 0,
+        name: "name",
+        html_url: "html_url",
+        description: "description",
+        stargazers_count: 0
+      }
+    ]
+
+    render(ReposView, "repos.json", repos: repos)
+  end
 end
