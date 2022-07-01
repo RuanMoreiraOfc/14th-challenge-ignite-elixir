deb4d2f0ecef64d8d76c0e42b52bf7e3816a152a
Ruan Moreira de Jesus <ruanmoreiraofc@hotmail.com>
2022-07-01 12:34:49 -0300

    feat(UsersController): add `login` handler

--- lib/expi_web/controllers/users_controller.ex
+++ lib/expi_web/controllers/users_controller.ex
@@ -16,4 +16,12 @@ defmodule ExpiWeb.UsersController do
       |> render("create.json", user: user, token: token)
     end
   end
+
+  def login(conn, params) do
+    with {:ok, token} <- Guardian.authenticate(params) do
+      conn
+      |> put_status(:ok)
+      |> render("login.json", token: token)
+    end
+  end
 end
