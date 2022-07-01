f809f3febeabfc403aa9904fe1821410d1ee6894
Ruan Moreira de Jesus <ruanmoreiraofc@hotmail.com>
2022-07-01 13:08:48 -0300

    feat(UsersController): add `show` handler

--- lib/expi_web/controllers/users_controller.ex
+++ lib/expi_web/controllers/users_controller.ex
@@ -2,7 +2,7 @@ defmodule ExpiWeb.UsersController do
   use ExpiWeb, :controller
 
   alias Expi.User
-  alias Expi.Users.Create
+  alias Expi.Users.{Create, Get}
   alias ExpiWeb.Auth.Guardian
   alias ExpiWeb.FallbackController
 
@@ -22,6 +22,14 @@ defmodule ExpiWeb.UsersController do
       conn
       |> put_status(:ok)
       |> render("login.json", token: token)
+    end
+  end
+
+  def show(conn, %{"id" => id}) do
+    with {:ok, user} <- Get.by_id(id) do
+      conn
+      |> put_status(:ok)
+      |> render("user.json", user: user)
     end
   end
 end
