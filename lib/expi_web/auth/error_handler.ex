70bbdf34433b35d68301de87d0cd07d240537c69
Ruan Moreira de Jesus <ruanmoreiraofc@hotmail.com>
2022-07-01 12:28:41 -0300

    feat: create `Auth` error handler

---
+++ lib/expi_web/auth/error_handler.ex
@@ -0,0 +1,13 @@
+defmodule ExpiWeb.Auth.ErrorHandler do
+  @behaviour Guardian.Plug.ErrorHandler
+
+  alias Plug.Conn
+
+  def auth_error(conn, {error, _reason}, _opts) do
+    body = Jason.encode!(%{message: to_string(error)})
+
+    conn
+    |> Conn.put_resp_content_type("application/json")
+    |> Conn.send_resp(401, body)
+  end
+end
