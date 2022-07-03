6a91f67e593b3a3f3eaf08549a16132893694f83
Ruan Moreira de Jesus <ruanmoreiraofc@hotmail.com>
2022-07-02 22:55:11 -0300

    feat: add revoke token in pipeline(with database validation)

--- lib/expi_web/plugs/refresh_token.ex
+++ lib/expi_web/plugs/refresh_token.ex
@@ -10,6 +10,7 @@ defmodule ExpiWeb.Plugs.RefreshToken do
     ["Bearer " <> old_token] = get_req_header(conn, "authorization")
 
     {:ok, new_token} = Guardian.refresh_token(old_token)
+    {:ok, _claims} = Guardian.revoke(old_token)
 
     put_private(conn, :expi_new_token, new_token)
   end
